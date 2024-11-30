0dbcba770c83cc884359841ca814ce49d4d8c2d0
omerunsal <47754244+omerunsal@users.noreply.github.com>
2024-11-30 22:24:39 +0300

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by omer on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestAppTests/TestAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TestAppTests.swift
+//  TestAppTests
+//
+//  Created by omer on 30.11.2024.
+//
+
+import Testing
+
+struct TestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
